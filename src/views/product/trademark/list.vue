84e412d1d1e6031882e43e331befb5f8843290d0
3D1Y <hi_xieyufeng@163.com>
2020-12-12 08:34:17 +0800

    first

---
+++ src/views/product/trademark/list.vue
@@ -0,0 +1,239 @@
+<template>
+  <div>
+    <el-button type="primary" @click="visible = true" icon="el-icon-plus"
+      >添加</el-button
+    >
+
+    <el-table :data="trademarkList" border style="width: 100%; margin: 20px 0">
+      <el-table-column type="index" label="序号" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="tmName" label="品牌名称"> </el-table-column>
+      <el-table-column label="品牌LOGO">
+        <template slot-scope="scope">
+          <!-- {{ JSON.stringify(scope) }} -->
+          <!--
+            scope代表所有数据
+              scope.row 代表当前行所有数据
+          -->
+          <img class="trademark-img" :src="scope.row.logoUrl" alt="logo" />
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template>
+          <el-button type="warning" icon="el-icon-edit">修改</el-button>
+          <el-button type="danger" icon="el-icon-delete">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- <el-pagination
+      class="trademark-pagination"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :page-sizes="[3, 6, 9]"
+      :page-size.sync="limit"
+      :current-page="page"
+      layout="prev, pager, next, jumper, sizes, total"
+      :total="total"
+    >
+    </el-pagination> -->
+
+    <!--
+      :page-size.sync="limit"   可以让limit更新变成同步更新
+      :current-page.sync="page" 可以让page更新变成同步更新
+
+      $event
+        1.  在DOM事件中代表 event
+        <button @click="handle(123, $event)"></button>
+        触发事件是浏览器的行为，所以$event代表event
+
+        2. 在自定义事件中代表 第一个参数
+          <button @aaa="handle($event)"></button>
+          假设这样触发自定义事件： this.$emit('aaa', 123, 456);
+          那么$event就为123（第一个参数）
+    -->
+    <el-pagination
+      class="trademark-pagination"
+      @size-change="getPageList(page, $event)"
+      @current-change="getPageList($event, limit)"
+      :page-sizes="[3, 6, 9]"
+      :page-size.sync="limit"
+      :current-page="page"
+      layout="prev, pager, next, jumper, sizes, total"
+      :total="total"
+    >
+    </el-pagination>
+
+    <el-dialog title="添加品牌" :visible.sync="visible" width="50%">
+      <el-form
+        :model="trademarkForm"
+        :rules="rules"
+        ref="trademarkForm"
+        label-width="100px"
+      >
+        <el-form-item label="品牌名称" prop="tmName">
+          <el-input v-model="trademarkForm.tmName"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" prop="logoUrl">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img
+              v-if="trademarkForm.logoUrl"
+              :src="trademarkForm.logoUrl"
+              class="avatar"
+            />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+          <span>只能上传jpg/png文件，且不超过50kb</span>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="visible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm('trademarkForm')"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+// import { trademark } from "@/api";
+
+export default {
+  name: "TrademarkList",
+  data() {
+    return {
+      trademarkList: [], // 所有数据
+      total: 0, // 总数
+      page: 1, // 页码
+      limit: 3, // 每页条数
+      visible: false, // 对话框显示&隐藏
+      trademarkForm: {
+        // 表单数据
+        tmName: "",
+        logoUrl: "",
+      },
+      rules: {
+        // 表单校验规则
+        tmName: [
+          {
+            // 必填项
+            required: true,
+            // 错误信息
+            message: "请输入品牌名称",
+            // 触发表单校验时机
+            trigger: "blur",
+          },
+        ],
+        logoUrl: [
+          { required: true, message: "请上传品牌LOGO", trigger: "change" },
+        ],
+      },
+    };
+  },
+  methods: {
+    // 提交表单
+    submitForm(form) {
+      // 校验表单
+      this.$refs[form].validate((valid) => {
+        if (valid) {
+          // 表单校验通过
+          console.log(this.trademarkForm);
+        }
+      });
+    },
+    // 上传图片成功的回调
+    handleAvatarSuccess() {},
+    // 上次图片之前触发的回调
+    beforeAvatarUpload() {},
+    // handleSizeChange(limit) {
+    //   this.getPageList(this.page, limit);
+    // },
+    // handleCurrentChange(page) {
+    //   this.getPageList(page, this.limit);
+    // },
+    // 请求分页列表数据
+    async getPageList(page, limit) {
+      try {
+        const result = await this.$API.trademark.getPageList(page, limit);
+        if (result.code === 200) {
+          this.$message.success("获取品牌分页列表成功");
+          this.limit = result.data.size; // 代表每页显示的条数
+          this.page = result.data.current; // 代表当前页码
+          this.trademarkList = result.data.records;
+          this.total = result.data.total; // 总数
+        } else {
+          this.$message.error("获取品牌分页列表失败");
+        }
+      } catch (e) {
+        console.log(e);
+        this.$message.error("获取品牌分页列表失败");
+      }
+    },
+  },
+  mounted() {
+    this.getPageList(this.page, this.limit);
+  },
+};
+
+/*
+  lang="less"
+    完整写法
+    深度选择器 /deep/
+  lang="sass"
+    可以省略 {}
+    可以省略 ;
+    深度选择器 /deep/  >>>
+  lang="scss"
+    完整写法
+
+  lang="stylus"
+    可以省略 {}
+    可以省略 :
+    可以省略 ;
+
+  scoped
+    让样式只在当前组件生效
+    问题：加上scoped，会让设置的子组件样式失效
+    解决：使用深度选择器
+*/
+</script>
+
+<style lang="sass" scoped>
+.trademark-img
+  width: 150px
+
+.trademark-pagination
+  text-align: right
+
+>>>.el-pagination__sizes
+  margin-left: 250px
+
+>>>.avatar-uploader .el-upload
+  border: 1px dashed #d9d9d9
+  border-radius: 6px
+  cursor: pointer
+  position: relative
+  overflow: hidden
+  &:hover
+    border-color: #409EFF
+
+>>>.avatar-uploader-icon
+  font-size: 28px
+  color: #8c939d
+  width: 178px
+  height: 178px
+  line-height: 178px
+  text-align: center
+
+>>>.avatar
+  width: 178px
+  height: 178px
+  display: block
+</style>
